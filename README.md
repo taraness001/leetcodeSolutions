f08f1a7ad6cc2723c221f9c57e4bd21057803596
taraness <gkdrmf23@gmail.com>
2019-12-03 17:25:33 +0900

    import group-anagrams problem solving

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 
-1. isomorphic-strings : https://github.com/taraness001/leetcodeSolutions/tree/master/src/main/java/isomorphic_strings
+
+49. group-anagrams : https://github.com/taraness001/leetcodeSolutions/tree/master/src/main/java/group_anagrams
+205. isomorphic-strings : https://github.com/taraness001/leetcodeSolutions/tree/master/src/main/java/isomorphic_strings
--- src/main/java/group_anagrams/GroupAnagrams.java
+++ src/main/java/group_anagrams/GroupAnagrams.java
@@ -1,61 +1,30 @@
 package group_anagrams;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class GroupAnagrams {
 
+
 	public List<List<String>> groupAnagrams(String[] strs) {
-		List<List<String>> retList = new ArrayList<>();
-		List<Map<Character, Integer>> anagramList = new ArrayList<>();
+		Map<String, List<String>> retList = new HashMap<>();
 
 		for (String str :  strs) {
-			Map<Character, Integer> anagramMap = new HashMap<>();
 			char[] strArray = str.toCharArray();
+			Arrays.sort(strArray);
+			String sortString = String.valueOf(strArray);
 
-			for (char c : strArray) {
-				if (anagramMap.containsKey(c)) {
-					anagramMap.put(c, anagramMap.get(c) + 1);
-				}
-				else {
-					anagramMap.put(c, 1);
-				}
+			if (retList.containsKey(sortString)) {
+				retList.get(sortString).add(str);
+			}
+			else {
+				List<String> insertList = new ArrayList<>();
+				insertList.add(str);
+				retList.put(sortString, insertList);
 			}
 
-			boolean insertFlag = true;
-			for (int index = 0; index < anagramList.size(); index++) {
-				Map<Character, Integer> listMap = anagramList.get(index);
-				if (diffMap(anagramMap, listMap)) {
-					insertFlag = false;
-					retList.get(index).add(str);
-				}
-			}
-
-			if (insertFlag) {
-				anagramList.add(anagramMap);
-				List<String> newList = new ArrayList<>();
-				newList.add(str);
-				retList.add(newList);
-			}
 		}
 
-		return retList;
-	}
-
-
-	public boolean diffMap (Map<Character, Integer> anagramMap, Map<Character, Integer> listMap) {
-		for (Map.Entry<Character, Integer> listMapEntry : listMap.entrySet()) {
-			if (!anagramMap.containsKey(listMapEntry.getKey()) || !anagramMap.get(listMapEntry.getKey()).equals(listMapEntry.getValue())) return false;
-		}
-
-
-
-
-
-
-		return true;
+		return new ArrayList<>(retList.values());
 	}
 
 }
